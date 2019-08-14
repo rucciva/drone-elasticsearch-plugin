f56377b5d8384a8a7c3a34807d222b2cc80a3fb7
Rucciva <rucciva@gmail.com>
2019-08-14 12:10:33 +0700

    refactor settings

--- script.sh
+++ script.sh
@@ -8,12 +8,12 @@ export DRONE_BUILD_FINISHED_DATE=`date -d "@$DRONE_BUILD_FINISHED" +'%Y/%m/%d %H
 export DRONE_JOB_STARTED_DATE=`date -d "@$DRONE_JOB_STARTED" +'%Y/%m/%d %H:%M:%S'`
 export DRONE_JOB_FINISHED_DATE=`date -d "@$DRONE_JOB_FINISHED" +'%Y/%m/%d %H:%M:%S'`
 
-index_postfix=`eval $PLUGIN_ELASTICSEARCH_INDEX_POSTFIX_COMMAND`
-url="${ELASTICSEARCH_URL:-$PLUGIN_ELASTICSEARCH_URL}/${PLUGIN_ELASTICSEARCH_INDEX}${index_postfix}/doc"
+index_postfix=`eval $PLUGIN_INDEX_POSTFIX_COMMAND`
+url="${ELASTICSEARCH_URL:-$PLUGIN_URL}/${PLUGIN_INDEX}${index_postfix}/doc"
 body=`jq -c -n 'env' | jq -c 'with_entries( select( .key | contains("DRONE_") ) )' | jq -c 'with_entries(.key |= ascii_downcase)'`
 curl \
   -s \
-  -u "${ELASTICSEARCH_USERNAME:-$PLUGIN_ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD:-$PLUGIN_ELASTICSEARCH_PASSWORD}" \
+  -u "${ELASTICSEARCH_USERNAME:-$PLUGIN_USERNAME}:${ELASTICSEARCH_PASSWORD:-$PLUGIN_PASSWORD}" \
   -d "$body" \
   -H "Content-Type: application/json"  \
   "$url"
