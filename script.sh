04baf79a7fb0263ea2fd32376d0b0c61b0f1b4f4
Rucciva <rucciva@gmail.com>
2020-06-02 16:14:51 +0700

    fix deprecated drone env

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ set -e
 export DRONE_BUILD_CREATED_DATE=`date -d "@$DRONE_BUILD_CREATED" +'%Y/%m/%d %H:%M:%S'`
 export DRONE_BUILD_STARTED_DATE=`date -d "@$DRONE_BUILD_STARTED" +'%Y/%m/%d %H:%M:%S'`
 export DRONE_BUILD_FINISHED_DATE=`date -d "@$DRONE_BUILD_FINISHED" +'%Y/%m/%d %H:%M:%S'`
-export DRONE_JOB_STARTED_DATE=`date -d "@$DRONE_JOB_STARTED" +'%Y/%m/%d %H:%M:%S'`
-export DRONE_JOB_FINISHED_DATE=`date -d "@$DRONE_JOB_FINISHED" +'%Y/%m/%d %H:%M:%S'`
+export DRONE_JOB_STARTED_DATE=`date -d "@${DRONE_JOB_STARTED:-$DRONE_BUILD_STARTED}" +'%Y/%m/%d %H:%M:%S'`
+export DRONE_JOB_FINISHED_DATE=`date -d "@${DRONE_JOB_FINISHED:-$DRONE_BUILD_FINISHED}" +'%Y/%m/%d %H:%M:%S'`
 
 index_postfix=`eval "$PLUGIN_INDEX_POSTFIX_COMMAND || echo"`
 url="${ELASTICSEARCH_URL:-$PLUGIN_URL}/${PLUGIN_INDEX}${index_postfix}/doc"
